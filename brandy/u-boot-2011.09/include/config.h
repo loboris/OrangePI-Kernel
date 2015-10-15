6484de70d68c45b1b69c6f92fdf902ec9790882c
loboris <loboris.gmail.com>
2015-10-16 00:00:59 +0200

    some fixes

---
+++ brandy/u-boot-2011.09/include/config.h
@@ -0,0 +1,6 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/sunxi/sun8iw7
+#include <config_cmd_defaults.h>
+#include <config_defaults.h>
+#include <configs/sun8iw7p1.h>
+#include <asm/config.h>
---
+++ brandy/u-boot-2011.09/include/config.mk
@@ -0,0 +1,6 @@
+TARGET = sun8iw7p1
+ARCH   = arm
+CPU    = armv7
+BOARD  = sun8iw7
+VENDOR = sunxi
+SOC    = sun8iw7
